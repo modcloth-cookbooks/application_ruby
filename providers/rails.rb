02f082fc5afa5097fdda812986a62ac887407caa
Eric Saxby <sax@livinginthepast.org>
2012-08-22 15:24:05 -0700

    Rails deployment always bundles production gems

--- providers/rails.rb
+++ providers/rails.rb
@@ -68,7 +68,7 @@ action :before_migrate do
     link "#{new_resource.release_path}/vendor/bundle" do
       to "#{new_resource.path}/shared/vendor_bundle"
     end
-    common_groups = %w{development test cucumber staging production}
+    common_groups = %w{development test cucumber staging}
     common_groups += new_resource.bundler_without_groups
     common_groups -= [new_resource.environment_name]
     common_groups = common_groups.join(' ')
@@ -77,18 +77,10 @@ action :before_migrate do
       # Check for a Gemfile.lock
       bundler_deployment = ::File.exists?(::File.join(new_resource.release_path, "Gemfile.lock"))
     end
-    bash 'XXXXXX bundle install' do
-      cwd "/root"
-      user "root"
-      #environment new_resource.environment
-      # flags '-l -c'
-      code <<-EOH
-      echo 'trying to run bundle install & sleep '
-      # su - ecomm -c bash -l 'cd /home/ecomm/ && bundle install --path=vendor/bundle #{bundler_deployment ? "--deployment " : ""}--without #{common_groups} && rbenv rehash'
-       su - ecomm -c 'bash -l -c "cd /home/ecomm/current && bundle install --path=vendor/bundle --deployment --without development test cucumber staging production && rbenv rehash" '
-       touch /tmp/algo
-      EOH
-     #only_if { ::File.exist?("/home/ecomm/current") }
+    execute "bundle install --path=vendor/bundle #{bundler_deployment ? "--deployment " : ""}--without #{common_groups}" do
+      cwd new_resource.release_path
+      user new_resource.owner
+      environment new_resource.environment
     end
   else
     # chef runs before_migrate, then symlink_before_migrate symlinks, then migrations,
