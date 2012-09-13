40b26ce6179ce9a4f4e06bdd9ba81fe689510759
Ben Marini <bmarini@gmail.com>
2012-09-13 15:22:14 -0700

    Add ability to set pool config in database.yml

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.8"
+version          "1.0.9"
 
 %w{ application runit unicorn apache2 passenger_apache2 smf }.each do |cb|
   depends cb
